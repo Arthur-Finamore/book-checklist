fc4c54897a00bc534153c2df29dacf81f2ea84d9
Arthur Finamore <arthurfinamore@gmail.com>
2023-09-16 20:08:10 -0300

    create new view
    created and configurated the kit book new view, still need to adjust css

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,53 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @items = Item.all
   end
 
   def show
-
+    # Use o @item definido pelo before_action para exibir os detalhes do item
   end
 
   def new
-
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
 
+    if @item.save
+      redirect_to @item, notice: 'Item foi criado com sucesso.'
+    else
+      render :new
+    end
   end
 
   def edit
-
+    # Use o @item definido pelo before_action para editar o item
   end
 
   def update
-
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item foi atualizado com sucesso.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_url, notice: 'Item foi excluído com sucesso.'
+  end
 
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    # Defina os parâmetros permitidos aqui com base nos campos do modelo Item
+    params.require(:item).permit(:item, :quantity, :supplier, :unit_price, :total_price, :shipping, :observation)
   end
 end
--- app/controllers/kit_books_controller.rb
+++ app/controllers/kit_books_controller.rb
@@ -1,31 +1,65 @@
 class KitBooksController < ApplicationController
+  before_action :set_kit_book, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @kit_books = KitBook.all
   end
 
   def show
-
+    # Mostrar detalhes do kit de livros
   end
+
 
   def new
     @kit_book = KitBook.new
-    2.times { @kit_book.items.build }
+     2.times { @kit_book.items.build } # Construir 2 itens iniciais
+    @item_number = 1
+
+    # Remove the empty div from the DOM
+    if @kit_book.items.count == 1
+      @kit_book.items[0].destroy
+    end
   end
 
   def create
+    @kit_book = KitBook.new(kit_book_params)
+    @kit_book.name = Book.find(kit_book_params[:book_id]).title if kit_book_params[:book_id].present?
+    puts "Params received: #{params.inspect}" # Debugging output
 
+    if @kit_book.save
+      redirect_to @kit_book, notice: 'Kit Book criado com sucesso.'
+    else
+      puts "Validation Errors: #{@kit_book.errors.full_messages}"
+        # render :new
+    end
   end
 
-  def edit
 
+  def edit
+    # Editar o kit de livros
   end
 
   def update
-
+    if @kit_book.update(kit_book_params)
+      redirect_to @kit_book, notice: 'Kit de livros atualizado com sucesso.'
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @kit_book.destroy
+    redirect_to kit_books_url, notice: 'Kit de livros excluído com sucesso.'
   end
+
+  private
+
+  def set_kit_book
+    @kit_book = KitBook.find(params[:id])
+  end
+
+  def kit_book_params
+    params.require(:kit_book).permit(:book_id, :name, items_attributes: [:id, :item, :quantity, :supplier, :unit_price, :total_price, :shipping, :observation, :_destroy])
+  end
+
 end
