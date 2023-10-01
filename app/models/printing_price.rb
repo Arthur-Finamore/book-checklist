88dd94b7cf78d2a371f6eeb1300cbeb4e147b19d
Arthur Finamore <arthurfinamore@gmail.com>
2023-10-01 13:13:21 -0300

    added Santa Marta and Pancron prices
    Added the Santa Marta and Pancron attributes to the printing prices model and added them to all the places the other prices are shown

--- app/models/printing_price.rb
+++ app/models/printing_price.rb
@@ -1,3 +1,12 @@
 class PrintingPrice < ApplicationRecord
   belongs_to :book
+
+  validates :santa_marta_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :pancron_price, numericality: { greater_than_or_equal_to: 0 }
+
+  # Atributos existentes
+  validates :ipsis_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :coan_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :geografica_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :leograf_price, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_183208) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_01_154820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_183208) do
     t.bigint "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "santa_marta_price"
+    t.decimal "pancron_price"
     t.index ["book_id"], name: "index_printing_prices_on_book_id"
   end
 
